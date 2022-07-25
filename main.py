ace3222e2152a6150b4d7a0b734f61958f259e63
Cristian Cortes Mondragon <ccortesm2000@gmail.com>
2022-07-24 20:00:19 -0500

    http_exception

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 # Python
-
-
-
-
-
-
 from doctest import Example
-from msilib.schema import File
-
 from typing import Optional
 from enum import Enum
-
-from urllib import response
-
-
-
-
 
 # Pydantic
 from pydantic import EmailStr
@@ -25,6 +11,7 @@ from pydantic import Field
 # FastAPI
 from fastapi import FastAPI
 from fastapi import status
+from fastapi import HTTPException
 from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 
@@ -128,7 +115,7 @@ def show_person(
     return {name: age}
 
 # Validaciones # Path operations
-
+persons = [1, 2, 3, 4, 5]
 @app.get("/person/detail/{person_id}")
 def show_person(
     person_id: int = Path(
@@ -139,6 +126,11 @@ def show_person(
         example=45
         )
 ):
+    if person_id not in persons:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail= 'this person does not exist'
+        )
     return {person_id: "It exists!"}
 
 # validaciones : request body
